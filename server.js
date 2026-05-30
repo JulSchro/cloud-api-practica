6a5cde70d0b7a09b693dcb3d00459f92df2d9a66
Diego Aaron Romero Lizarraga <romerolizarragadiego007@gmail.com>
2026-05-29 19:41:52 -0600

    Corrige variable PORT duplicada

--- server.js
+++ server.js
@@ -9,7 +9,6 @@ const PORT = process.env.PORT || 3000;
 app.use(express.json());
 app.use(express.static("public"));
 
-const PORT = process.env.PORT || 3000;
 // Coordenadas simples de algunas ciudades
 const ciudades = {
   "tampico": {
